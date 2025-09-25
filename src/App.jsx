6967a91313b94299529d56db44b350219ec73b5f
Niraj1903 <niraj.singh1903@outlook.com>
2025-09-25 22:20:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-red-600 flex justify-center m-2">
+        Autocomplete Search Bar
+      </h1>
+    </>
+  );
+}
+
+export default App;
